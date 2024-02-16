37b58c67199815c9fdfb78d4bce9be8012a45fea
Isaac <isaacy@gmail.com>
2024-02-15 19:27:09 -0600

    linted - added semicolon

--- server/database/app.js
+++ server/database/app.js
@@ -6,7 +6,7 @@ const  cors = require('cors');
 const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
